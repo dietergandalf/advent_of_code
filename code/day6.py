92bb1cde8f750e6497bd8d26bcd44a86d58de499
Malte <maltebothe@web.de>
2022-12-06 13:40:25 +0100

    day6 with else

--- code/day6.py
+++ code/day6.py
@@ -3,16 +3,16 @@ def find_first_x_nonduplicate_characters(string: str, x: int):
     """Find the first x non-duplicate characters in a string."""
     for i in range(len(string)-(x+1)):
         s = string[i:i+x]
-        count = 0
         for c in s:
-            if s.count(c) == 1:
-                count += 1
-            if count == x:
-                print(i+x)
-                return string[i:i+x]
+            if s.count(c) != 1:
+                break
+        else:
+            print(i+x, end=" ")
+            return string[i:i+x]
     return None
     
 
 with open("input/day6.txt") as f:
     data = f.read()
+    print(find_first_x_nonduplicate_characters(data, 4))
     print(find_first_x_nonduplicate_characters(data, 14))
